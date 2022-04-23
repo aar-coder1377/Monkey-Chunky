58bc6bbeb24fbdf963c63de9c55c7d814681d539
Aarnav Gurung <codingurung.gmail.com>
2022-04-23 15:34:34 -0700

    add a text component

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ export default class App extends React.Component {
             style: { color: '#fff', fontSize: 20 },
           }}
         />
-
+      <Text> Hello</Text>
         <Image
           style={styles.imageIcon}
           source={{
